5673b8b3cfe2d19c0a89a2a6145be098c36e1963
João Souza <j.gabrieltec@gmail.com>
2025-10-05 23:41:31 -0300

    feat: update witch shortcuts to remove for black hole

--- nvim/lua/config/keymaps.lua
+++ nvim/lua/config/keymaps.lua
@@ -3,3 +3,26 @@
 -- Add any additional keymaps here
 vim.api.nvim_set_keymap("i", "jj", "<Esc>", { noremap = false })
 vim.api.nvim_set_keymap("i", "jk", "<Esc>", { noremap = false })
+
+-- =========================================
+-- 🔥 Delete com Black Hole (sem copiar)
+-- =========================================
+
+local wk = require("which-key")
+
+wk.add({
+  { "<leader>D", group = "🗑 Delete (black hole)" },
+
+  { "<leader>Dd", '"_dd', desc = "Delete line" },
+  { "<leader>Dw", '"_dw', desc = "Delete word" },
+  { "<leader>Ds", '"_d', mode = "v", desc = "Delete selection" },
+
+  -- 🔽 Delete até o final do arquivo
+  { "<leader>De", '"_dG', desc = "🔽 Delete until end of file" },
+
+  -- 🔼 Delete até o início do arquivo
+  { "<leader>Di", '"_dgg', desc = "🔼 Delete until start of file" },
+
+  -- 💣 Delete tudo no arquivo
+  { "<leader>Da", 'gg"_dG', desc = "💣 Delete entire file" },
+})
