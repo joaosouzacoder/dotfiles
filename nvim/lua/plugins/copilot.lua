d367aa08003a0b3d026579291fb6823a98c19714
João Souza <j.gabrieltec@gmail.com>
2025-10-07 13:11:34 -0300

    feat: add copilot.vim

--- nvim/lua/plugins/copilot.lua
+++ nvim/lua/plugins/copilot.lua
@@ -1,16 +1,6 @@
 return {
   {
-    "zbirenbaum/copilot.lua",
-    cmd = "Copilot",
-    event = "VeryLazy",
-    opts = {
-      suggestion = { enabled = false },
-      panel = { enabled = false },
-      filetypes = {
-        markdown = true,
-        help = true,
-      },
-    },
+    "github/copilot.vim",
   },
 
   {
