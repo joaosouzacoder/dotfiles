2c27d3ab5eb9b5af87c79d66e9952a1a0b95fc7e
João Souza <j.gabrieltec@gmail.com>
2025-10-08 00:59:04 -0300

    feat: add inc rename with noice integration

--- nvim/lua/plugins/noice.lua
+++ nvim/lua/plugins/noice.lua
@@ -7,13 +7,18 @@ return {
           enabled = true, -- ou false se quiser desativar totalmente
         },
       },
+      presets = {
+        inc_rename = true, -- habilita o comando de renomeação incremental
+      },
       cmdline = {
         view = "cmdline",
       },
     },
     config = function(_, opts)
       require("noice").setup(opts)
-
+      vim.keymap.set("n", "<leader>rn", function()
+        return ":IncRename " .. vim.fn.expand("<cword>")
+      end, { expr = true, desc = "Rename symbol (incremental)" })
       -- 🛠️ Patch para corrigir erros de progresso com Roslyn
       vim.lsp.handlers["$/progress"] = function(_, result, ctx)
         local ok, noice = pcall(require, "noice.lsp.progress")
